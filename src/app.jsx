4f88ff25fac0ce4bfa8fdce7ef545b5c5eee431e
Lizh <15156692746@163.com>
2018-05-21 13:53:21 +0800

    初始化

---
+++ src/app.jsx
@@ -0,0 +1,24 @@
+/*
+* @Author: Lizh
+* @Date:   2018-05-20 23:06:56
+* @Last Modified by:   Lizh
+* @Last Modified time: 2018-05-21 11:19:52
+*/
+// react 
+import React from 'react';
+// react-dom
+import ReactDOM from 'react-dom';
+// CSS
+import './index.css';
+// SASS
+import './index.scss';
+// font-awesome 字体
+import 'font-awesome/css/font-awesome.min.css';
+
+ReactDOM.render(
+	<div>
+	<i class="fa fa-address-book"> </i>
+    <h1>Hello, world!~</h1>
+    </div>,
+  document.getElementById('app')
+);
